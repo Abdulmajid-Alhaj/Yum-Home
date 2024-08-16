02d4702cebf789b796d5d759ef7f9f6994c1a759
Abdulmajid-Alhaj <abdemaged.alhaj123@gmail.com>
2024-08-16 19:45:03 +0300

    abd

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser')
 const connectedDB = require('./db/connectedDB')
 const auth = require('./routers/authRouter')
+const detail = require('./routers/detailRouter')
+const admin = require('./routers/adminRouter')
+const menu = require('./routers/menuRouter');
+
 const app = express()
 
 app.use(express.json())
@@ -14,6 +18,9 @@ app.use(cors({
 app.use(cookieParser())
 
 app.use('/api/yum', auth)
+app.use('/api/yum', menu)
+app.use('/api/yum' , admin)
+app.use('/api/yum', detail)
 
 app.listen(process.env.PORT || 5000 , () => {
     connectedDB()
